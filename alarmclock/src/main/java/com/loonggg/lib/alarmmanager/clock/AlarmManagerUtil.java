cb5460e0e5828e64d906be06a0611ff9a45afbb9
Thomas Liu <liuxk99@gmail.com>
2019-11-29 18:04:38 +0800

    <feature>: check in and checkout remindering

--- alarmclock/src/main/java/com/loonggg/lib/alarmmanager/clock/AlarmManagerUtil.java
+++ alarmclock/src/main/java/com/loonggg/lib/alarmmanager/clock/AlarmManagerUtil.java
@@ -87,7 +87,6 @@ public class AlarmManagerUtil {
         }
     }
 
-
     /**
      * @param weekflag 传入的是周几
      * @param dateTime 传入的是时间戳（设置当天的年月日+从选择框拿来的时分秒）
@@ -136,5 +135,45 @@ public class AlarmManagerUtil {
         return time;
     }
 
+    /**
+     * @param flag            周期性时间间隔的标志,flag = 0 表示一次性的闹钟, flag = 1 表示每天提醒的闹钟(1天的时间间隔),flag = 2
+     *                        表示按周每周提醒的闹钟（一周的周期性时间间隔）
+     * @param timeInMillis    闹钟的开始时间
+     * @param id              闹钟的id
+     * @param week            week=0表示一次性闹钟或者按天的周期性闹钟，非0 的情况下是几就代表以周为周期性的周几的闹钟
+     * @param tips            闹钟提示信息
+     * @param soundOrVibrator 2表示声音和震动都执行，1表示只有铃声提醒，0表示只有震动提醒
+     */
+    public static void setAlarm(Context context, int flag, long timeInMillis, int id, int
+            week, String tips, int soundOrVibrator) {
+        AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        long intervalMillis = 0;
 
+        if (flag == 0) {
+            intervalMillis = 0;
+        } else if (flag == 1) {
+            intervalMillis = 24 * 3600 * 1000;
+        } else if (flag == 2) {
+            intervalMillis = 24 * 3600 * 1000 * 7;
+        }
+        Intent intent = new Intent(ALARM_ACTION);
+        intent.putExtra("intervalMillis", intervalMillis);
+        intent.putExtra("msg", tips);
+        intent.putExtra("id", id);
+        intent.putExtra("soundOrVibrator", soundOrVibrator);
+        PendingIntent sender = PendingIntent.getBroadcast(context, id, intent, PendingIntent
+                .FLAG_CANCEL_CURRENT);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Log.d(TAG, "date-time: " + TimeUtils.formatDateTime(timeInMillis));
+
+            am.setWindow(AlarmManager.RTC_WAKEUP, timeInMillis,
+                    intervalMillis, sender);
+        } else {
+            if (flag == 0) {
+                am.set(AlarmManager.RTC_WAKEUP, timeInMillis, sender);
+            } else {
+                am.setRepeating(AlarmManager.RTC_WAKEUP, timeInMillis, intervalMillis, sender);
+            }
+        }
+    }
 }
--- alarmclock/src/main/java/com/loonggg/lib/alarmmanager/clock/ClockAlarmActivity.java
+++ alarmclock/src/main/java/com/loonggg/lib/alarmmanager/clock/ClockAlarmActivity.java
@@ -54,8 +54,5 @@ public class ClockAlarmActivity extends Activity {
                 }
             }
         });
-
-
     }
-
 }
